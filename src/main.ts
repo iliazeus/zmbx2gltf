218d235227481e99cd0d940be6a65c957e5d929f
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2023-01-13 17:44:28 +0600

    remove commander dependency for browser compat

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,30 @@ try {
   require("source-map-support/register");
 } catch (error) {}
 
-import { program } from "commander";
+// import { program } from "commander";
 import * as jszip from "jszip";
 
 import { readFile, writeFile } from "fs/promises";
 
 import { convertMbxToGltf } from "./convert";
 
-program
-  .name("zmbx2gltf")
-  .argument("<input-file>")
-  .argument("<output-file>")
-  .action(async (inputFile: string, outputFile: string) => {
-    const zmbxZip = await jszip.loadAsync(await readFile(inputFile));
+const main = async (args: string[]) => {
+  if (args.length !== 2) {
+    throw new Error("usage: zmbx2gltf <input.zmbx> <output.gltf>");
+  }
 
-    const mbxString = await zmbxZip.file("scene.mbx")?.async("string");
-    if (!mbxString) throw new Error("invalid file format");
+  const [inputFile, outputFile] = args;
 
-    const gltf = convertMbxToGltf(JSON.parse(mbxString));
-    await writeFile(outputFile, JSON.stringify(gltf), "utf-8");
-  });
+  const zmbxZip = await jszip.loadAsync(await readFile(inputFile));
 
-program.parseAsync().catch((error) => {
+  const mbxString = await zmbxZip.file("scene.mbx")?.async("string");
+  if (!mbxString) throw new Error("invalid file format");
+
+  const gltf = convertMbxToGltf(JSON.parse(mbxString));
+  await writeFile(outputFile, JSON.stringify(gltf), "utf-8");
+};
+
+main(process.argv.slice(2)).catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
